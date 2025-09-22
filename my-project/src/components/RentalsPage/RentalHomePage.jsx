7df7efcb4a25351b4d886100ccab2a2b1a387642
CodeWithTammy <andersontamera1969@gmail.com>
2025-09-22 15:34:38 -0500

    rental media query mod

--- my-project/src/components/RentalsPage/RentalHomePage.jsx
+++ my-project/src/components/RentalsPage/RentalHomePage.jsx
@@ -57,7 +57,7 @@ const RentalHomePage = () => {
             </select> */}
           </div>
 
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+          <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
               {products.map((product) => (
                   <RentalCard
                       key={product._id}
